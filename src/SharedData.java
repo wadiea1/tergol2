6eeeecba8811732f85463d40d3a02cc8a09a8900
wadiea123 <wadiea951@gmail.com>
2024-11-20 16:42:50 +0200

    second commit

---
+++ src/SharedData.java
@@ -0,0 +1,44 @@
+public class SharedData 
+{
+	private int [] array;
+	private boolean [] winArray;
+	private boolean flag;
+	private final int b;
+	private int t;
+	
+	public SharedData(int[] array, int b) {
+		
+		this.array = array;
+		this.b = b;
+	}
+
+	public boolean[] getWinArray() 
+	{
+		return winArray;
+	}
+
+	public void setWinArray(boolean [] winArray) 
+	{
+		this.winArray = winArray;
+	}
+
+	public int[] getArray() 
+	{
+		return array;
+	}
+
+	public int getB() 
+	{
+		return b;
+	}
+
+	public boolean getFlag() 
+	{
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+}
